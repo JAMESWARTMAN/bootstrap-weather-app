619e434b33af30ed5fad0f3ba1637bf14930802d
jameswartman <jameswartmanmichael@yahoo.com>
2022-01-04 11:16:35 -0500

    first commit

---
+++ fetch.js
@@ -0,0 +1,17 @@
+class Fetch {
+    async getCurrent(input) {
+      const myKey = "88375fd2f2059f3bd20fb6524f179495";
+  
+      //make request to url
+  
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+      );
+  
+      const data = await response.json();
+  
+      console.log(data);
+  
+      return data;
+    }
+  }
